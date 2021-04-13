e63bb446fe2b18d81ab77e61d1f91250e6ef0870
Tawfik <tawfikabdeldaim@gmail.com>
2021-04-13 14:01:25 +0400

    firstCommmit

---
+++ hello.py
@@ -0,0 +1,9 @@
+msg = 'Hello World 5'
+print (msg)
+import matplotlib.pyplot as plt
+import numpy as np
+x = np.linspace(0, 20, 100)  # Create a list of evenly-spaced numbers over the range
+plt.plot(x, np.sin(x))       # Plot the sine of each x point
+plt.show()                   # Display the plotgit --version
+msg = ('Bye worlrd')
+msg = ('Bye worlrd')
